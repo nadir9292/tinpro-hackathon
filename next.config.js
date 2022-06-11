107f16c56ec26ff3170506c4ae63f1bb6a2c0ba5
nadir9292 <nadirbtssio@gmail.com>
2022-06-11 16:21:19 +0200

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "https://image.similarpng.com/very-thumbnail/2021/05/Crown-icon-design-on-transparent-background-PNG.png",
-    ],
-  },
 }
 
-export default nextConfig
+module.exports = nextConfig
